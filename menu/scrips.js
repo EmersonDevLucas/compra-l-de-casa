37668a68e2b804ffdd762feb2cca8c1a0a774480
Emerson Souza <emersonlucasts@gmail.com>
2024-06-29 03:25:45 -0300

    Upadate code pages

---
+++ menu/scrips.js
@@ -0,0 +1,99 @@
+
+document.getElementById('novacompra').onclick = function novacompra(){
+    window.location.replace('http://127.0.0.1:5500/novacompra/')
+}
+
+document.getElementById('compra').onclick = function compra(){
+    window.location.replace('http://127.0.0.1:5500/compras/')
+}
+
+let dataAtual = new Date();
+new Date().getDay;
+let dia = dataAtual.getDay();
+let mes = dataAtual.getMonth();
+let mes2 = dataAtual.getDate();
+
+switch (dia) {
+  case 0:
+    dia = "Dom";
+    break;
+
+  case 1:
+    dia = "Seg";
+    break;
+
+  case 2:
+    dia = "Ter";
+    break;
+
+  case 3:
+    dia = "Qua";
+    break;
+
+  case 4:
+    dia = "Qui";
+    break;
+
+  case 5:
+    dia = "Sex";
+    break;
+
+  case 6:
+    dia = "Sab";
+    break;
+}
+
+switch (mes) {
+  case 0:
+    mes = "Jan";
+    break;
+
+  case 1:
+    mes = "Fev";
+
+  case 2:
+    mes = "Mar";
+    break;
+
+  case 3:
+    mes = "Abr";
+    break;
+
+  case 4:
+    mes = "Mai";
+    break;
+
+  case 5:
+    mes = "Jun";
+    break;
+
+  case 6:
+    mes = "Jul";
+    break;
+
+    case 7: 
+    mes = 'Ago';
+    break;
+
+    case 8: 
+    mes = 'Set';
+    break;
+
+    case 9: 
+    mes = 'Out';
+    break;
+
+    case 10: 
+    mes = 'Nov';
+    break;
+
+    case 11: 
+    mes = 'Dez';
+    break;
+}
+
+
+
+document.getElementById("dia").innerHTML = dia;
+document.getElementById("mes").innerHTML = mes;
+document.getElementById("dianumber").innerHTML = mes2;
---
+++ novacompra/script.js
@@ -0,0 +1,14 @@
+document.getElementById('novoitembutton').onclick =
+function () {
+    let novoitem = document.getElementById('novoitem')
+
+    novoitem.style.display = 'flex'
+}
+
+document.getElementById('salvaritem').onclick =
+function () {
+    let nomeitem = document.getElementById('nomeitem')
+    let quantiadeitem = document.getElementById('quantiadeitem')
+    
+    
+}
